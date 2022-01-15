90c11c5f50da659c3ad4c60b3d926e935fc4df24
rafaelmenegazzi <rafael.menegazzi.usp@gmail.com>
2022-01-14 23:09:24 -0300

    first commit nest start with nestjs/mongoose

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/costumer-orders'),
+    ProductsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
